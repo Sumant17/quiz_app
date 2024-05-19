be083be524a6708317436716151ad390c1875b09
Ganesh <ganeshbhat015@gmail.com>
2024-05-19 17:34:14 +0530

    Implement timer and razorpay

--- lib/components/auth/auth.dart
+++ lib/components/auth/auth.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:quiz_app/components/auth/auth_bloc.dart';
@@ -5,10 +7,37 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:quiz_app/components/email_auth/email.dart';
 import 'package:quiz_app/components/email_auth/email_signup.dart';
 import 'package:quiz_app/components/quiz_page/landing_page.dart';
+import 'package:razorpay_flutter/razorpay_flutter.dart';
 
-class Auth extends StatelessWidget {
-  late AuthBloc authBloc;
+class Auth extends StatefulWidget {
   Auth({super.key});
+
+  @override
+  State<Auth> createState() => _AuthState();
+}
+
+class _AuthState extends State<Auth> {
+  late AuthBloc authBloc;
+  late int _secondsRemaining = 60;
+  late Timer _timer;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+
+    // start timer
+    _timer = Timer.periodic(Duration(seconds: 1), (timer) {
+      if (_secondsRemaining == 0) {
+        _timer.cancel();
+      } else {
+        setState(() {
+          _secondsRemaining--;
+        });
+      }
+    });
+
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -32,7 +61,23 @@ class Auth extends StatelessWidget {
           if (state is LoadAuth) {
             final isSignIn = state.isSigin;
             return Scaffold(
-              appBar: AppBar(),
+              appBar: AppBar(
+                title: const Text('Quiz App'),
+                actions: [
+                  Row(
+                    children: [
+                      Icon(Icons.timer),
+                      const SizedBox(
+                        width: 10,
+                      ),
+                      Text('$_secondsRemaining'),
+                      const SizedBox(
+                        width: 10,
+                      )
+                    ],
+                  )
+                ],
+              ),
               body: Padding(
                 padding: const EdgeInsets.all(8.0),
                 child: Center(
@@ -98,6 +143,29 @@ class Auth extends StatelessWidget {
                         child: OutlinedButton(
                           onPressed: () {
                             // authBloc.add(OnAuthClicked);
+                            // razorpay
+                            Razorpay razorpay = Razorpay();
+                            var options = {
+                              'key': 'rzp_test_dcpEg18EO9ge1q',
+                              'amount': 20000,
+                              'name': 'ABC Corp.',
+                              'description': 'Fine T-Shirt',
+                              'retry': {'enabled': true, 'max_count': 1},
+                              'send_sms_hash': true,
+                              'external': {
+                                'wallets': ['paytm']
+                              }
+                            };
+                            // _timer.cancel();
+
+                            razorpay.on(Razorpay.EVENT_PAYMENT_ERROR,
+                                handlePaymentErrorResponse);
+                            razorpay.on(Razorpay.EVENT_PAYMENT_SUCCESS,
+                                handlePaymentSuccessResponse);
+                            razorpay.on(Razorpay.EVENT_EXTERNAL_WALLET,
+                                handleExternalWalletSelected);
+
+                            razorpay.open(options);
                           },
                           style: OutlinedButton.styleFrom(
                             backgroundColor: Colors.white,
@@ -167,5 +235,43 @@ class Auth extends StatelessWidget {
     print(value.data);
     Navigator.of(context)
         .push(MaterialPageRoute(builder: (context) => const LandingPage()));
+  }
+
+  handlePaymentErrorResponse() {
+    print("Payment error");
+  }
+
+  void handlePaymentSuccessResponse(PaymentSuccessResponse response) {
+    /*
+    * Payment Success Response contains three values:
+    * 1. Order ID
+    * 2. Payment ID
+    * 3. Signature
+    * */
+    print(response.data.toString());
+    showAlertDialog(
+        context, "Payment Successful", "Payment ID: ${response.paymentId}");
+  }
+
+  handleExternalWalletSelected() {}
+
+  void showAlertDialog(BuildContext context, String title, String message) {
+    // set up the buttons
+    Widget continueButton = ElevatedButton(
+      child: const Text("Continue"),
+      onPressed: () {},
+    );
+    // set up the AlertDialog
+    AlertDialog alert = AlertDialog(
+      title: Text(title),
+      content: Text(message),
+    );
+    // show the dialog
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return alert;
+      },
+    );
   }
 }
